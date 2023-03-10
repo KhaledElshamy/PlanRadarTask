90697b047c8a089a00e3d4593a9b07b95ab2fdfc
Khaled Ahmed Elshamy <khaledelshamy094@outlook.com>
2023-03-10 15:05:33 +0200

    first commit

---
+++ PlanRadarTask/Component/BaseVC.swift
@@ -0,0 +1,8 @@
+//
+//  BaseVC.swift
+//  PlanRadarTask
+//
+//  Created by Khaled Elshamy on 10/03/2023.
+//
+
+import Foundation
---
+++ PlanRadarTask/Utilites/ReusableView.swift
@@ -0,0 +1,8 @@
+//
+//  ReusableView.swift
+//  PlanRadarTask
+//
+//  Created by Khaled Elshamy on 10/03/2023.
+//
+
+import Foundation
